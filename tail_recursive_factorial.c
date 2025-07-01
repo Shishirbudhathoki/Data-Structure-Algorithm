e94ca64b252a666a1a72853f4cbc88ca6e2ef987
Shishir Budhathoki <shishirbudhathoki4000@gmail.com>
2025-07-01 07:39:06 +0545

    Added Code to calculate factorial in c using tail recurrsion

--- tail_recursive_factorial.c
+++ tail_recursive_factorial.c
@@ -6,10 +6,10 @@ long int tailFact(int n, long int a){
     else
     return tailFact(n-1,a*n);
 }
-int main(){
+int main(){ 
     int n;
     printf("Enter the value of n : \n");
     scanf("%d",&n);
-    printf("The factorial of %d using tail recursion is %ld",n,tailFact(n,1));
+    printf("The factorial of %d using tail recursion is %ld .",n,tailFact(n,1));
     return 0;
 }
