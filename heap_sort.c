85d3277b28ebffcbc4381b28452154a3e7ad3681
Shishir Budhathoki <shishirbudhathoki4000@gmail.com>
2025-08-16 07:07:31 +0545

    Code to sort data using heap sort at index 1.

---
+++ heap_sort.c
@@ -0,0 +1,51 @@
+// for index 1
+#include<stdio.h>
+#include<conio.h>
+void heap_sort(int arr[],int n);
+void build_max_heap(int arr[],int n);
+void max_heaping(int arr[],int i ,int n){
+    
+    int l=2*i,r=2*i+1,largest=i,temp;
+    if(l<=n && arr[l]>arr[largest])
+        largest=l;
+    if(r<=n && arr[r]>arr[largest])
+        largest=r;
+    if(i!=largest)
+    {
+        temp=arr[i];
+        arr[i]=arr[largest];
+        arr[largest]=temp;
+        max_heaping(arr,largest,n);
+    }
+}
+void build_max_heap(int arr[],int n)
+{
+    int i;
+    for(i=n/2;i>=1;i--)
+        max_heaping(arr,i,n);  
+}
+void heap_sort(int arr[],int n)
+{
+    int i,temp;
+    build_max_heap(arr,n);
+    for(i=n;i>=2;i--)
+    {
+        temp=arr[1];
+        arr[1]=arr[i];
+        arr[i]=temp;
+        n=n-1;
+        max_heaping(arr,1,n);
+    }
+}
+int main()
+{
+    int arr[] = {0,11,17,19,100,99,27,7,3,13};
+    int i,n = sizeof(arr) / sizeof(arr[0]);    
+    printf("The size of array is : %d\n",n);
+    heap_sort(arr,n-1);
+    printf("Sorted array: ");
+    for (i = 1; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    return 0;
+}
